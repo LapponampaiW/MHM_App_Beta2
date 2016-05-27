493c7cbd57d033736690b685a3007a38069bcc8b
ball <ballz_v@hotmail.com>
2016-05-27 23:39:44 +0700

    เสร็จ 4 ปุ่ม pop up(2)

--- app/src/main/java/com/su/lapponampai_w/mhm_app_beta1/MainActivity.java
+++ app/src/main/java/com/su/lapponampai_w/mhm_app_beta1/MainActivity.java
@@ -58,9 +58,6 @@ public class MainActivity extends AppCompatActivity {
         clickAddbtn();
 
 
-
-
-
         buttonAddMedicine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {

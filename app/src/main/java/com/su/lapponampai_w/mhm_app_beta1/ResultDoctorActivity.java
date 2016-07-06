6452452f63926bdd86545ecbe90e3f2bcf2b5968
ball <ballz_v@hotmail.com>
2016-07-06 16:59:36 +0700

    6 july

--- app/src/main/java/com/su/lapponampai_w/mhm_app_beta1/ResultDoctorActivity.java
+++ app/src/main/java/com/su/lapponampai_w/mhm_app_beta1/ResultDoctorActivity.java
@@ -1,6 +1,7 @@
 package com.su.lapponampai_w.mhm_app_beta1;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.AsyncTask;
 import android.os.DropBoxManager;
 import android.support.v7.app.AppCompatActivity;
@@ -17,6 +18,9 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -134,8 +138,17 @@ public class ResultDoctorActivity extends AppCompatActivity {
 
                 Log.d("6JulyV2", "JSON ==>" + jsonString);
 
-            }
+
+                    updateNewMainTABLE(jsonString);
+
+
+            } //onResponse
         }));
+
+
+
+
+
 
         //SynMainTABLE synMainTABLE = new SynMainTABLE(this, emailString, urlMainSTRING);
         //synMainTABLE.execute();
@@ -143,5 +156,77 @@ public class ResultDoctorActivity extends AppCompatActivity {
 
 
     } //checkEmail
+
+    private void updateNewMainTABLE(String jsonString) {
+
+
+        String strJSON = jsonString;
+        Log.d("6JulyV3", "JSON at Update ==>" + strJSON);
+
+        if (strJSON.length() != 4) {
+            //email True
+            Log.d("6JulyV3", "Email True");
+
+            deleteMainTABLE();
+
+            updateValueFromServerToMain(strJSON);
+
+
+        } else {
+
+            Log.d("6JulyV3", "Email False");
+        }
+
+
+    } //Update
+
+    private void updateValueFromServerToMain(String strJSON) {
+
+        try {
+
+            JSONArray jsonArray = new JSONArray(strJSON);
+            for(int i=0;i<jsonArray.length();i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                String strID = jsonObject.getString("id");
+                /*
+                String strID = jsonObject.getString("id");
+                String strID = jsonObject.getString("id");
+                String strID = jsonObject.getString("id");
+                String strID = jsonObject.getString("id");
+                String strID = jsonObject.getString("id");
+                String strID = jsonObject.getString("id");
+                String strID = jsonObject.getString("id");
+                String strID = jsonObject.getString("id");
+                String strID = jsonObject.getString("id");
+                String strID = jsonObject.getString("id");
+                String strID = jsonObject.getString("id");
+                String strID = jsonObject.getString("id");
+                String strID = jsonObject.getString("id");
+                String strID = jsonObject.getString("id");
+                String strID = jsonObject.getString("id");
+                String strID = jsonObject.getString("id");
+                String strID = jsonObject.getString("id");
+                */
+
+                MyManage myManage = new MyManage(this);
+                //myManage.addValueTomainTABLE();
+
+
+
+            }
+
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteMainTABLE() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyHelper.DATABASE_NAME, MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.mainTABLE, null, null);
+    }
 
 } //Main Class

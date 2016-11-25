8c38e2814db967d819316debe56c0cb4db6c8e12
ball <ballz_v@hotmail.com>
2016-11-25 15:57:43 +0700

    (25/11/2559) ==>> ทำ PopUpChangeImage (2)

--- app/src/main/java/com/su/lapponampai_w/mhm_app_beta1/PopUpChangeAppearance.java
+++ app/src/main/java/com/su/lapponampai_w/mhm_app_beta1/PopUpChangeAppearance.java
@@ -3,16 +3,77 @@ package com.su.lapponampai_w.mhm_app_beta1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PopUpChangeAppearance extends AppCompatActivity {
+    
+    
+    //Explicit
+    ImageView imageView1,imageView2, imageView3;
+    TextView textViewStep2Tablet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop_up_change_appearance);
 
-
+        bindWidget();
+        
         displayMetrics();
+
+        showView();
+
+        clickImageView(); //คลิก ImageView เปลี่ยนสี
+        
+        
+    }
+
+    private void clickImageView() {
+
+        imageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                imageView1.setImageResource(R.drawable.icon_tablet2);
+                imageView2.setImageResource(R.drawable.icon_capsule1);
+
+            }
+        });
+
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                imageView1.setImageResource(R.drawable.icon_tablet1);
+                imageView2.setImageResource(R.drawable.icon_capsule2);
+
+            }
+        });
+
+        imageView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(),"ยังไม่มีฟังก์ชั่นยาน้ำในเว่อร์ชั่นนี้",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void showView() {
+        textViewStep2Tablet.setText("ขั้นตอนที่ 2 :\nรูปแบบยาเม็ด");
+    }
+
+    private void bindWidget() {
+
+        imageView1 = (ImageView) findViewById(R.id.imageView18);
+        imageView2 = (ImageView) findViewById(R.id.imageView19);
+        imageView3 = (ImageView) findViewById(R.id.imageView20);
+
+        textViewStep2Tablet = (TextView) findViewById(R.id.textView209);
+        
     }
 
     private void displayMetrics() {
